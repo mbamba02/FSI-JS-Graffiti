a6db5b0618ffab296412a4fa31fad3a335acad3e
Mamadou Bamba <mybamba@gmail.com>
2022-04-25 18:45:23 -0400

    Title to left images radius

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header'); //# to select id
+header.style.textAlign = "left";
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
